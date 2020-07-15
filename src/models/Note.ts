c9cbb4acea5dc2594b03cf666fc0590a9e44e065
denisshav <denis.shavshin@gmail.com>
2020-07-15 17:29:13 +0300

    Refractor notes api

--- src/models/Note.ts
+++ src/models/Note.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose"
 
 export const noteSchema = new mongoose.Schema({
+  _id: String,
   title: String,
   content: String,
   folder: String,
   date: Date,
+  userId: String
 })
 
 const Note = mongoose.model("Note", noteSchema)
